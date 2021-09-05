7a6ee351d8fb3a1ad9eae2b0ec17174728604e2e
Alberto Gatti <gattimei@gmail.com>
2021-09-05 23:52:19 +0200

    tavern UI

---
+++ LisergyClient/LisergyClient/Assets/Code/Tavern/TavernMaskUIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.Tavern
+{
+    public class TavernMaskUIController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private GridLayoutGroup gridLayoutGroup;
+        [SerializeField] private GameObject[] buttons;
+
+        private int currentUnit;
+
+        private void Start()
+        {
+            CheckUnits();
+        }
+
+        private void OnEnable()
+        {
+            CheckUnits();
+        }
+
+        private void CheckUnits()
+        {
+            currentUnit = GetComponentsInChildren<UnitGridItem>().Length;
+            RectTransform rt = rectTransform;
+
+            if (currentUnit > 7)
+            {
+                rt.sizeDelta = new Vector2((210 * currentUnit) + gridLayoutGroup.padding.horizontal, rt.sizeDelta.y);
+                foreach (var button in buttons)
+                    button.SetActive(true);
+            }
+            else
+            {
+                foreach (var button in buttons)
+                    button.SetActive(false);
+                rt.sizeDelta = new Vector2(1560, rt.sizeDelta.y);
+            }
+        }
+    }
+}
--- LisergyServer/Game/Town/TownPacketListener.cs
+++ LisergyServer/Game/Town/TownPacketListener.cs
@@ -39,17 +39,102 @@ namespace Game.Listeners
                 player = p.Sender;
                 var unit = new Unit();
                 unit.Name = "Lancelot";
-                unit.Stats.HP = 20;
+                unit.Stats.HP = 5;
                 unit.Flavour = "Famous Knight, Lancelot strives for battles and action.";
                 unit.Sprite = "knight";
                 player.Units.Add(unit);
 
                 unit = new Unit();
+                unit.Name = "Gawain";
+                unit.Stats.HP = 5;
+                unit.Flavour = "Knights of the Round Table, Gawain strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Geraint";
+                unit.Stats.HP = 1;
+                unit.Flavour = "Knights of the Round Table, Geraint strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Percival";
+                unit.Stats.HP = 3;
+                unit.Flavour = "Knights of the Round Table, Percival strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Bors";
+                unit.Stats.HP = 5;
+                unit.Flavour = "Knights of the Round Table, Bors strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Lamorak";
+                unit.Stats.HP = 2;
+                unit.Flavour = "Knights of the Round Table, Lamorak strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Tristran";
+                unit.Stats.HP = 3;
+                unit.Flavour = "Knights of the Round Table, Tristran strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Kay";
+                unit.Stats.HP = 5;
+                unit.Flavour = "Knights of the Round Table, Kay strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Gareth";
+                unit.Stats.HP = 1;
+                unit.Flavour = "Knights of the Round Table, Gareth strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Bedivere";
+                unit.Stats.HP = 4;
+                unit.Flavour = "Knights of the Round Table, Bedivere strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Gaheris";
+                unit.Stats.HP = 1;
+                unit.Flavour = "Knights of the Round Table, Gaheris strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "Galahad";
+                unit.Stats.HP = 3;
+                unit.Flavour = "Knights of the Round Table, Galahad strives for battles and action.";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
+                unit = new Unit();
                 unit.Name = "Merlin";
-                unit.Stats.HP = 10;
+                unit.Stats.HP = 2;
                 unit.Flavour = "Famous Wizard, Merlin have lived his life in search for magical powers";
                 unit.Sprite = "mage";
                 player.Units.Add(unit);
+
+                unit = new Unit();
+                unit.Name = "King Arthur";
+                unit.Stats.HP = 2;
+                unit.Flavour = "Famous King, Arthur constructs a round table, at which only the best knights may sit";
+                unit.Sprite = "knight";
+                player.Units.Add(unit);
+
                 Game.Chain.UpdatePlayer(player);
             }
             Log.Debug($"{player} joined");
